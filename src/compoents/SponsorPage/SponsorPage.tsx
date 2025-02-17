fad9970ff09738dbe1b713a436b7c4ea541211ab
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-02-17 20:04:26 +0800

    remove banned payment methods

--- src/compoents/SponsorPage/SponsorPage.tsx
+++ src/compoents/SponsorPage/SponsorPage.tsx
@@ -3,10 +3,8 @@ import { useTranslation } from 'react-i18next';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import wechatQR from '../../assets/wechat-qr.webp';
-import alipayQR from '../../assets/alipay-qr.webp';
 import paypalQR from '../../assets/paypal-qr.webp';
 import TonQR from '../../assets/TON-qr.webp';
-import USDTTRC20QR from '../../assets/USDT-TRC20-qr.webp';
 import Divider from '../Divider/Divider.tsx';
 import SocialLinks from '../SocialLinks/SocialLinks.tsx';
 
@@ -55,11 +53,6 @@ function SponsorPage() {
                             alt="WeChat QR"
                         />
                         <PaymentMethod
-                            title={t('sponsor.alipay')}
-                            img={alipayQR}
-                            alt="Alipay QR"
-                        />
-                        <PaymentMethod
                             title="PayPal"
                             img={paypalQR}
                             alt="PayPal QR"
@@ -70,11 +63,6 @@ function SponsorPage() {
                             title="TON"
                             img={TonQR}
                             alt="TON QR"
-                        />
-                        <PaymentMethod
-                            title="USDT(TRC20)"
-                            img={USDTTRC20QR}
-                            alt="USDT TRC20 QR"
                         />
                     </div>
                 </section>
